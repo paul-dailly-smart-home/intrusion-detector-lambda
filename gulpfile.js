c9d5c10975f6ec0dac53f3dbb0b569f71af6a291
Paul Dailly <paul.dailly@mttnow.com>
2016-06-29 22:09:17 +0100

    Added sudo false flag to travis config to run on container based infrastructure Added gulp and configured to produce mocha and istanbul code coverage

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
+
+
+gulp.task('test', function (cb) {
+  gulp.src(['./src/**/*.js', '!./src/**/*.spec.js'])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+    .on('finish', function () {
+      gulp.src(['./src/**/*.spec.js'])
+        .pipe(mocha({reporter: 'mochawesome', reporterOptions: {
+          reportDir: 'build/reports/unit-test',
+          reportName: 'unit-test-report'
+        }}))
+        .pipe(istanbul.writeReports({dir: 'build/coverage/unit-test'}))
+        .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }))
+        .on('end', cb);
+    });
+});
--- src/eventsources/kinesisEventSource.spec.js
+++ src/eventsources/kinesisEventSource.spec.js
@@ -6,9 +6,9 @@ describe('Kinesis motion detected event processing', ()=> {
   it('decodes the motion detected events', (done) => {
 
     const kinesisEvent = createKinesisEvent();
-
+    
     kinesisEventSource.motionEventConsumer(kinesisEvent, {}, (err, decodedEvents) => {
-      expect(decodedEvents.length).to.eql(3);
+      expect(decodedEvents.length).to.eql(2);
       expect(decodedEvents[0].cameraId).to.eql('camera3');
       expect(decodedEvents[1].cameraId).to.eql('camera1');
       done();
