248cce4ff3473304f61e574d5f2c2a6796645c7a
Paul Dailly <paul.dailly@mttnow.com>
2016-07-02 15:57:31 +0100

    Adding all env args to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo $DEPLOYMENT_ENV
-./node_modules/node-lambda/bin/node-lambda deploy -a $AWS_DEPLOY_USER_ACCESS_KEY -s $AWS_DEPLOY_USER_SECRET_KEY --role arn:aws:iam::810905322061:role/lambda_kinesis_role -e $DEPLOYMENT_ENV --configFile deploy.env
+./node_modules/node-lambda/bin/node-lambda deploy -a $AWS_DEPLOY_USER_ACCESS_KEY -s $AWS_DEPLOY_USER_SECRET_KEY --role arn:aws:iam::810905322061:role/lambda_kinesis_role -e $DEPLOYMENT_ENV --configFile deploy.env -n motionEventsProcessor --handler src/eventsources/kinesisEventSource.motionEventConsumer -x "event.json **/*.spec.js src/test .DS_Store .idea"
