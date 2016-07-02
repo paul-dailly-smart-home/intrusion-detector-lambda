8b9bd4139c58aeab7f6a3011c312cdb8d5c37b05
Paul Dailly <paul.dailly@mttnow.com>
2016-07-03 00:42:52 +0100

    Fixed casing issue decoding dynamoDb event object

--- src/eventsources/dynamoDbEventSource.js
+++ src/eventsources/dynamoDbEventSource.js
@@ -1,6 +1,7 @@
 exports.motionEventConsumer =  (event, context) => {
 
-  event.Records.map((record) => {
+  event.Records.forEach((record) => {
+    console.log(`Record: ${JSON.stringify(record)}`);
     var eventDecoded = new Buffer(record.dynamodb.NewImage.event_raw.B, 'base64').toString('ascii');
     var eventId = record.dynamoDb.NewImage.eventId.S;
     console.log(`DynamoDB motion detected event with id ${eventId} and payload ${eventDecoded}`);
