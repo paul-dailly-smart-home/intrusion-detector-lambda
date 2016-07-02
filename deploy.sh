457d74597bde271a59ec5b404cad22b944b5bea2
Paul Dailly <paul.dailly@mttnow.com>
2016-07-02 16:45:20 +0100

    Removed deploy.env reference in deploy script and added lambda function description

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./node_modules/node-lambda/bin/node-lambda deploy -a $AWS_DEPLOY_USER_ACCESS_KEY -s $AWS_DEPLOY_USER_SECRET_KEY --role arn:aws:iam::810905322061:role/lambda_kinesis_role -e $DEPLOYMENT_ENV --configFile deploy.env
+./node_modules/node-lambda/bin/node-lambda deploy -a $AWS_DEPLOY_USER_ACCESS_KEY -s $AWS_DEPLOY_USER_SECRET_KEY --role arn:aws:iam::810905322061:role/lambda_kinesis_role -e $DEPLOYMENT_ENV -d "Processes motion detected events from Kinesis stream (Travis build: $TRAVIS_BUILD_NUMBER, Commit id: $TRAVIS_COMMIT)"
