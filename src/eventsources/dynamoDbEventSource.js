e8009b79908724ffaa3fb929a7c7c040b586150c
Paul Dailly <paul.dailly@mttnow.com>
2016-07-03 00:30:20 +0100

    Added decoding of dynamoDb event payload

--- src/eventsources/dynamoDbEventSource.js
+++ src/eventsources/dynamoDbEventSource.js
@@ -1,8 +1,9 @@
 exports.motionEventConsumer =  (event, context) => {
 
   event.Records.map((record) => {
-    // Kinesis data is base64 encoded so decode here
-    console.log('DynamoDB motion detected event item: ', JSON.stringify(record));
+    var eventDecoded = new Buffer(record.dynamoDb.NewImage.event_raw, 'base64').toString('ascii');
+    var eventId = record.dynamoDb.NewImage.eventId.S;
+    console.log(`DynamoDB motion detected event with id ${eventId} and payload ${eventDecoded}`);
   });
   
 };
