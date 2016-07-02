95902300843f17ad10469d0dff4b8e3d05e3e9f6
Paul Dailly <paul.dailly@mttnow.com>
2016-07-02 15:42:47 +0100

    Added bash script for deployment from TravisCI

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../node_modules/node-lambda/bin/node-lambda deploy -a $AWS_DEPLOY_USER_ACCESS_KEY -s $AWS_DEPLOY_USER_SECRET_KEY --role arn:aws:iam::810905322061:role/lambda_kinesis_role -e $DEPLOYMENT_ENV --configFile deploy.env
