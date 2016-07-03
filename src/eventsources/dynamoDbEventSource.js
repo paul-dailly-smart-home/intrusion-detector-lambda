6216f9d2169d4b483e45c80b0c87851ab457dd63
Paul Dailly <paul.dailly@mttnow.com>
2016-07-03 16:02:15 +0100

    Adding debugging for dynamoDb events

--- src/eventsources/dynamoDbEventSource.js
+++ src/eventsources/dynamoDbEventSource.js
@@ -5,6 +5,7 @@ exports.motionEventConsumer = (event, context) => {
       console.log(`Ignoring event record with DynamoDb event type: ${record.eventName}`);
       return;
     }
+    console.log(`Processing event record ${JSON.stringify(record)}`);
     var eventDecoded = new Buffer(record.dynamodb.NewImage.event_raw.B, 'base64').toString('ascii');
     var eventId = record.dynamodb.NewImage.eventId.S;
     console.log(`DynamoDB motion detected event with id ${eventId} and payload ${eventDecoded}`);
