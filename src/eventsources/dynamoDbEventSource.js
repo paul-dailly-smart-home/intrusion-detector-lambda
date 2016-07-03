4bce82b3ea38e17ee8e5804dde1e97dce3b594cd
Paul Dailly <paul.dailly@mttnow.com>
2016-07-03 14:51:27 +0100

    Ingore all dynamo record event types other than inserts

--- src/eventsources/dynamoDbEventSource.js
+++ src/eventsources/dynamoDbEventSource.js
@@ -1,9 +1,12 @@
-exports.motionEventConsumer =  (event, context) => {
+exports.motionEventConsumer = (event, context) => {
 
   event.Records.forEach((record) => {
+    if (record.eventName !== 'INSERT') {
+      return;
+    }
     var eventDecoded = new Buffer(record.dynamodb.NewImage.event_raw.B, 'base64').toString('ascii');
     var eventId = record.dynamodb.NewImage.eventId.S;
     console.log(`DynamoDB motion detected event with id ${eventId} and payload ${eventDecoded}`);
   });
-  
+
 };
