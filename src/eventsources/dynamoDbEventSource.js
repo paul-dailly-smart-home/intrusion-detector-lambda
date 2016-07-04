9a14b2105bf10a2b403c04bdee925f4181d3a40e
Paul Dailly <paul.dailly@mttnow.com>
2016-07-04 21:59:21 +0100

    - Removed Kinesis event processing - Added unit tests for DynamoDB event processing

--- src/eventsources/dynamoDbEventSource.js
+++ src/eventsources/dynamoDbEventSource.js
@@ -1,14 +1,66 @@
-exports.motionEventConsumer = (event, context) => {
+const AWS = require('aws-sdk');
+const uuid = require('node-uuid');
+const INTRUSIONS_TABLE = 'Intrusions';
+const NOT_INSERT_EVENT_MESSAGE = 'DynamoDb event not of type INSERT';
+
+const _createIntrusionEntry = (motionDetectedEvent) => {
+  const intrusionId = uuid.v4();
+  var now = `${new Date().getTime()}`;
+  return {
+    id: {
+      S: intrusionId
+    },
+    tenantId: {
+      S: motionDetectedEvent.tenantId.S
+    },
+    propertyId: {
+      S: motionDetectedEvent.propertyId.S
+    },
+    zoneId: {
+      S: motionDetectedEvent.zoneId.S
+    },
+    sensorId: {
+      S: motionDetectedEvent.sensorId.S
+    },
+    createdTimestamp: {
+      N: now
+    },
+    acknowledgedTimestamp: {
+      N: now
+    },
+    lastFlaggedTimestamp: {
+      N: now
+    },
+    status: {
+      S: 'UNACKNOWLEDGED'
+    }
+  };
+};
+
+const _addIntrusionEntryToDynamo = (motionDetectedEvent, callback) => {
+  const db = new AWS.DynamoDB();
+  const intrusionEntry = _createIntrusionEntry(motionDetectedEvent);
+  const params = {
+    TableName: INTRUSIONS_TABLE,
+    Item: intrusionEntry
+  };
+  db.putItem(params, callback);
+};
+
+
+exports.motionEventConsumer = (event, context, callback) => {
 
   event.Records.forEach((record) => {
     if (record.eventName !== 'INSERT') {
       console.log(`Ignoring event record with DynamoDb event type: ${record.eventName}`);
-      return;
+      callback(new Error(NOT_INSERT_EVENT_MESSAGE));
     }
-    console.log(`Processing event record ${JSON.stringify(record)}`);
-    var eventDecoded = new Buffer(record.dynamodb.NewImage.event_raw.B, 'base64').toString('ascii');
-    var eventId = record.dynamodb.NewImage.eventId.S;
-    console.log(`DynamoDB motion detected event with id ${eventId} and payload ${eventDecoded}`);
+    // console.log(`Processing event record ${JSON.stringify(record)}`);
+    // var eventId = record.dynamodb.NewImage.eventId.S;
+    
+    // TODO: only insert entry if one does not already exist  
+    _addIntrusionEntryToDynamo(record.dynamodb.NewImage.event.M, callback);
   });
+
 
 };
--- src/eventsources/dynamoDbEventSource.spec.js
+++ src/eventsources/dynamoDbEventSource.spec.js
@@ -1,38 +1,114 @@
-var motionDetectedEvent = {
-  "eventId": "event1",
-  "tenantId": "tenant1",
-  "propertyId": "home1",
-  "zoneId": "zone1",
-  "sensorId": "motionSensor1",
-  "eventType": "MOTION_DETECTED",
-  "timestamp": "1467554109857"
-};
-var encodedMotionDetectedEvent = new Buffer(JSON.stringify(motionDetectedEvent)).toString('base64');
+const expect = require('chai').expect;
+const dynamoEventSource = require('./dynamoDbEventSource');
+const awsMock = require('aws-sdk-mock');
+const sinon = require('sinon');
+const uuid = require('node-uuid');
+const uniqueId = 'uniqueId';
+const now = new Date().getTime();
+var fakeClock;
+var sandbox;
 
-var dynamoDbRecord = {
-  "eventID": "2eddd37f8c3189ac070803b466742333",
-  "eventName": "INSERT",
-  "eventVersion": "1.1",
-  "eventSource": "aws:dynamodb",
-  "awsRegion": "eu-west-1",
-  "dynamodb": {
-    "ApproximateCreationDateTime": 1467501900,
-    "Keys": {
-      "eventId": {
-        "S": "event1"
-      }
-    },
-    "NewImage": {
-      "eventId": {
-        "S": "event1"
+describe('DynamoDb motion detected events processing', ()=> {
+  beforeEach(() => {
+    fakeClock = sinon.useFakeTimers(now);
+    sandbox = sinon.sandbox.create();
+    sandbox.stub(uuid, 'v4', ()=> uniqueId);
+
+  });
+
+  afterEach(()=> {
+    sandbox.restore();
+    fakeClock.restore();
+    awsMock.restore();
+  });
+
+  it('ignores all other dynamo events except INSERT', (done)=>{
+    const dynamoEvent = _createDynamoDbEventRecord('NOT_INSERT');
+    dynamoEventSource.motionEventConsumer({"Records": [dynamoEvent]}, null, (err)=> {
+      expect(err.message).to.eql('DynamoDb event not of type INSERT');
+      done();
+    });
+  });
+
+  it('creates an intrusion entry if one does not already exist', (done) => {
+    awsMock.mock('DynamoDB', 'putItem', (data, cb) => {console.log('mock called');cb(data.Item);});
+    var expectedIntrusionEntry = {
+      id: {S: uniqueId},
+      tenantId: {S: "tenant1"},
+      propertyId: {S: "home1"},
+      zoneId: {S: "zone1"},
+      sensorId: {S: "motionSensor1"},
+      createdTimestamp: {N: `${now}`},
+      acknowledgedTimestamp: {N: `${now}`},
+      lastFlaggedTimestamp: {N: `${now}`},
+      status: {S: "UNACKNOWLEDGED"}
+    };
+
+    const dynamoEvent = _createDynamoDbEventRecord('INSERT');
+
+    dynamoEventSource.motionEventConsumer({"Records": [dynamoEvent]}, null, (insertedItem)=> {
+      expect(insertedItem).to.eql(expectedIntrusionEntry);
+      done();
+    });
+  });
+  
+  // it('creates UNACKNOWLEDGED_INTRUSION_EVENT when intrusion last flagged time greater than configured period', ()=>{});
+  //
+  // it('updates last flagged timestamp of intrusion entry having created UNACKNOWLEDGED_INTRUSION_EVENT', ()=>{});
+  //
+  // it('handles multiple motion detected events in single invocation', ()=>{});
+
+});
+
+
+const _createDynamoDbEventRecord = (eventName) => {
+  return {
+    "eventID": "5e33be5c18d151fef734151b5235c245",
+    "eventName": eventName,
+    "eventVersion": "1.1",
+    "eventSource": "aws:dynamodb",
+    "awsRegion": "eu-west-1",
+    "dynamodb": {
+      "ApproximateCreationDateTime": 1467557940,
+      "Keys": {
+        "eventId": {
+          "S": "event4"
+        }
       },
-      "event_raw": {
-        "B": encodedMotionDetectedEvent
-      }
+      "NewImage": {
+        "eventId": {
+          "S": "event4"
+        },
+        "event": {
+          "M": {
+            "eventId": {
+              "S": "event4"
+            },
+            "tenantId": {
+              "S": "tenant1"
+            },
+            "zoneId": {
+              "S": "zone1"
+            },
+            "eventType": {
+              "S": "MOTION_DETECTED"
+            },
+            "propertyId": {
+              "S": "home1"
+            },
+            "timestamp": {
+              "S": "1467554109857"
+            },
+            "sensorId": {
+              "S": "motionSensor1"
+            }
+          }
+        }
+      },
+      "SequenceNumber": "3395600000000009508512985",
+      "SizeBytes": 162,
+      "StreamViewType": "NEW_IMAGE"
     },
-    "SequenceNumber": "855700000000007800281582",
-    "SizeBytes": 215,
-    "StreamViewType": "NEW_IMAGE"
-  },
-  "eventSourceARN": "arn:aws:dynamodb:eu-west-1:810905322061:table/MotionDetectedEvents/stream/2016-07-02T18:20:00.773"
+    "eventSourceARN": "arn:aws:dynamodb:eu-west-1:810905322061:table/MotionDetectedEvents/stream/2016-07-02T18:20:00.773"
+  };
 };
