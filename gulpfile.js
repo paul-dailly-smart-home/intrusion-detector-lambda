09f7085fe6d5b541283531c9cf5d074d498fc53d
Paul Dailly <paul.dailly@mttnow.com>
2016-07-06 21:45:20 +0100

    Removed XML unit test reporting

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,6 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
-const TEST_REPORT_FORMAT = process.env.TEST_REPORT_FORMAT == 'junitXml' ? 'junitXml' : 'html';
-const TEST_REPORT_DIR = process.env.TEST_REPORT_DIR || 'build/reports/unit-test/';
-const TEST_REPORTERS = {
-  junitXml: {
-    reporter: 'mocha-junit-reporter',
-    reporterOptions: {
-      mochaFile: TEST_REPORT_DIR + 'junit.xml'
-    }
-  },
-  html: {
-    reporter: 'mochawesome',
-    reporterOptions: {
-      reportDir: TEST_REPORT_DIR,
-      reportName: 'unit-test-report'
-    }
-  }
-};
-
-var _getReporterConfig = () => TEST_REPORTERS[TEST_REPORT_FORMAT];
 
 gulp.task('test', function (cb) {
   gulp.src(['./src/**/*.js', '!./src/**/*.spec.js'])
@@ -27,9 +8,12 @@ gulp.task('test', function (cb) {
     .pipe(istanbul.hookRequire())
     .on('finish', function () {
       gulp.src(['./src/**/*.spec.js'])
-        .pipe(mocha(
-          _getReporterConfig()
-        ))
+        .pipe(mocha({
+          reporter: 'mochawesome', reporterOptions: {
+            reportDir: 'build/reports/unit-test',
+            reportName: 'unit-test-report'
+          }
+        }))
         .pipe(istanbul.writeReports({dir: 'build/coverage/unit-test'}))
         .pipe(istanbul.enforceThresholds({thresholds: {global: 90}}))
         .on('end', cb);
